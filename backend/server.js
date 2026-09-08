7fac3f56706420241cd45abd91f885b5b66753cd
VISHNUPRIYAMUTHAIAH <vishnupriyathrisha@gmail.com>
2026-09-08 11:27:31 +0530

    Update CORS for production deployment

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,7 @@ const server = http.createServer(app);
 // Socket.IO
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL,
     methods: ["GET", "POST", "PUT", "DELETE"],
   },
 });
@@ -49,7 +49,11 @@ setIO(io);
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+  })
+);
 app.use(express.json());
 
 // Socket.IO Authentication
